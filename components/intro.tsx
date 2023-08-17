074426b207e58a818ced9021f3c977a51610a94c
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-08-17 17:31:24 +0200

    intro section created

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,9 +1,78 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import profileImg from "@/public/aleks.jpeg";
+
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section>
-      <div className="flex items-center"></div>
+    <section className="border border-1 border-red-500 mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ type: "spring", stiffness: 120 }}
+        >
+          <Image
+            className="h-48 w-48 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            src={profileImg}
+            alt="Aleksandar Ilic profile picture"
+            width="200"
+            height="200"
+            quality="95"
+            priority={true}
+          />
+        </motion.div>
+      </div>
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl"
+      >
+        Hello, I am Aleksandar Ilic. I love to code. I am a frontend web
+        developer. Please look at my portfolio and the projects i created!
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+      >
+        <Link
+          className="group flex items-center gap-2 bg-gray-900 text-white px-7 py-3 outline-none rounded-full focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          href="#contact"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          className="group cursor-pointer flex items-center gap-2 bg-white px-7 py-3 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+          href="/CV.pdf"
+          download={true}
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:opacity-100 transition" />
+        </a>
+        <a
+          className="cursor-pointer flex items-center gap-2 bg-white p-4 text-gray-700 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950 active:scale-105 transition border border-black/10"
+          href="https://linkedin.com/in/aleksandar-ilic-267a49260"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a className="cursor-pointer flex items-center gap-2 bg-white p-4 text-[1.35rem] text-gray-700 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950 active:scale-105 transition border border-black/10">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
