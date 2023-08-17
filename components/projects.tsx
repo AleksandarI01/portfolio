0d63a57e5a7cd067388446325085430c0cfa2fb6
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-08-17 18:42:23 +0200

    starting my projects section

---
+++ components/projects.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionTitle from "./section-title";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionTitle>My Projects</SectionTitle>
+    </section>
+  );
+}
