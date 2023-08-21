52fb1cf905a4bab71a9e05c79c733235aaad6d35
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-08-21 13:10:03 +0200

    transfering part of projects component to new component to make use server more available

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,8 +1,8 @@
-import React, { useRef } from "react";
+import React from "react";
 import SectionTitle from "./section-title";
 import { projectsData } from "@/lib/data";
-import Image from "next/image";
-import { useScroll } from "framer-motion";
+
+import Project from "./single-project";
 
 export default function Projects() {
   return (
@@ -14,39 +14,5 @@ export default function Projects() {
         ))}
       </div>
     </section>
-  );
-}
-
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  //const ref = useRef(null);
-
-  return (
-    <a href="https://dayventure.propulsion-learn.ch/" target="_blank">
-      <section className="relative group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 active:bg-gray-300 transition">
-        <div className="h-full pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <Image
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
-          src={imageUrl}
-          alt={`Website image of ${title}`}
-          quality={95}
-        />
-      </section>
-    </a>
   );
 }
